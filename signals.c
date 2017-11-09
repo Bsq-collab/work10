0183e80f0f9d111ce5e6fd535b6a35f7b4f5e2e4
Bayan Berri <bberri@stuy.edu>
2017-11-08 20:10:51 -0500

    idk why it isn't workin yet soon soon

--- signals.c
+++ signals.c
@@ -7,16 +7,25 @@
 #include <dirent.h>
 #include <signal.h>
 
-const char *FILE_PATH = "work10.txt";
-  
 static void sighandler(int signo){
   if(signo==SIGINT){
-    ""
+    char message[20]="byebyebyebye";
+    int filestuff=open("filestuff.txt",O_APPEND);
+    write(filestuff,message,sizeof(message));
+    close(filestuff);
+    exit(0);
+  }
+  if(signo==SIGUSR1){
+    printf("PID: %d\n",getppid());
   }
 }
+
 int main(){
+  signal(SIGINT,sighandler);
+  signal(SIGUSR1,sighandler);
+  
   while(1){
     printf("PID: %d\n", getpid());
-    sleep(1)
+    sleep(1);
   }
 }
